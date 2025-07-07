ceb5238adb5917d199372a18db8568e2b315c522
ezst036 <ezst036@gmx.com>
2025-07-07 00:56:08 -0400

    Script for enabling functionality

--- Selenium/admin/05enabledisablefunctionality.py
+++ Selenium/admin/05enabledisablefunctionality.py
@@ -70,3 +70,109 @@ time.sleep(3)
 defaultpreflink.click()
 
 time.sleep(5)
+
+driver.find_element(By.ID, "id_open_registration").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_intranet").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_connect").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_tithe").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_events").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_store").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_map").click()
+
+time.sleep(2)
+
+savelink = driver.find_element(By.NAME, "_save")
+savehover = ActionChains(driver).move_to_element(savelink)
+savehover.perform()
+time.sleep(3)
+
+savelink.click()
+
+viewsitelink = driver.find_element(By.LINK_TEXT, "VIEW SITE")
+viewsitehover = ActionChains(driver).move_to_element(viewsitelink)
+viewsitehover.perform()
+time.sleep(2)
+
+viewsitelink.click()
+
+time.sleep(5)
+
+hellolink = driver.find_element(By.LINK_TEXT, "Hello fakeadmin!")
+hellohover = ActionChains(driver).move_to_element(hellolink)
+hellohover.perform()
+time.sleep(3)
+
+hellolink.click()
+
+adminlink = driver.find_element(By.LINK_TEXT, "Administration")
+adminhover = ActionChains(driver).move_to_element(adminlink)
+adminhover.perform()
+time.sleep(3)
+
+adminlink.click()
+
+time.sleep(5)
+
+prefslink = driver.find_element(By.LINK_TEXT, "Preferences")
+prefshover = ActionChains(driver).move_to_element(prefslink)
+prefshover.perform()
+time.sleep(3)
+
+prefslink.click()
+
+time.sleep(5)
+
+defaultpreflink = driver.find_element(By.LINK_TEXT, "Open Check In")
+defaultprefhover = ActionChains(driver).move_to_element(defaultpreflink)
+defaultprefhover.perform()
+time.sleep(3)
+
+defaultpreflink.click()
+
+time.sleep(5)
+
+driver.find_element(By.ID, "id_store").click()
+
+time.sleep(2)
+
+driver.find_element(By.ID, "id_map").click()
+
+time.sleep(2)
+
+savelink = driver.find_element(By.NAME, "_save")
+savehover = ActionChains(driver).move_to_element(savelink)
+savehover.perform()
+time.sleep(3)
+
+savelink.click()
+
+viewsitelink = driver.find_element(By.LINK_TEXT, "VIEW SITE")
+viewsitehover = ActionChains(driver).move_to_element(viewsitelink)
+viewsitehover.perform()
+time.sleep(2)
+
+viewsitelink.click()
+
+time.sleep(8)
+
+link = driver.find_element(By.LINK_TEXT, "Logout")
+link.click()
+time.sleep(2)
